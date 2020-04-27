df869f84b3c0a139ef0990d857a80f8af5f1d3ba
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-27 22:52:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
 
-  def name=(dog_name)
-    this_dogs_name = dog_name
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
 
   def name
@@ -13,3 +13,19 @@ lassie = Dog.new
 lassie.name = "Lassie"
 
 puts lassie.name
+
+# class Dog
+# 
+#   def name=(dog_name)
+#     this_dogs_name = dog_name
+#   end
+# 
+#   def name
+#     @this_dogs_name
+#   end
+# end
+# 
+# lassie = Dog.new
+# lassie.name = "Lassie"
+# 
+# puts lassie.name
